95b96e679a63979ee7d9fcf9931e61d1e9105a44
Colton <colathro@microsoft.com>
2020-12-19 16:14:02 -0600

    cards update no this

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -1,60 +1,55 @@
 import React from "react";
 import { GameState } from "../states";
 import { observer } from "mobx-react-lite";
-import CardFlip from 'react-native-card-flip';
-import {
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import CardFlip from "react-native-card-flip";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export const Cards: React.FC = observer(
   (): JSX.Element => {
+    let card;
     return (
       <View style={styles.container}>
-        <CardFlip style={styles.cardContainer} ref={(card) => this.card = card} >
-          <TouchableOpacity 
-            style={styles.card} 
-            onPress={() => 
-              { 
-                if (GameState.activePhase == GameState.gamePhase.DRAW) {
-                  GameState.nextGamePhase();
-                  this.card.flip();
-                } else {
-                  this.card.tip();
-                }
-              }}>
-            <Text
-              style={styles.cardText}>
-              { 
-                GameState.activePhase == GameState.gamePhase.ROLL 
-                  ? ""
-                  :GameState.activePacks[GameState.activeDeck].name
+        <CardFlip
+          style={styles.cardContainer}
+          ref={(cardObj) => {
+            card = cardObj;
+          }}
+        >
+          <TouchableOpacity
+            style={styles.card}
+            onPress={() => {
+              if (GameState.activePhase == GameState.gamePhase.DRAW) {
+                GameState.nextGamePhase();
+                card.flip();
+              } else {
+                card.tip();
               }
+            }}
+          >
+            <Text style={styles.cardText}>
+              {GameState.activePhase == GameState.gamePhase.ROLL
+                ? ""
+                : GameState.activePacks[GameState.activeDeck].name}
             </Text>
           </TouchableOpacity>
-          <TouchableOpacity 
-            style={styles.flippedCard} 
-            onPress={() => 
-              {
-                GameState.nextGamePhase();
-                GameState.activePhase == GameState.gamePhase.ROLL 
-                  ? this.card.flip()
-                  : this.card.tip()
-              }} >
-            <Text
-              style={styles.prompt}
-            >
+          <TouchableOpacity
+            style={styles.flippedCard}
+            onPress={() => {
+              GameState.nextGamePhase();
+              GameState.activePhase == GameState.gamePhase.ROLL
+                ? card.flip()
+                : card.tip();
+            }}
+          >
+            <Text style={styles.prompt}>
               {GameState.activeCard.text != null
-                  ? GameState.activePacks[GameState.activeDeck].prompt
-                  : ""}
+                ? GameState.activePacks[GameState.activeDeck].prompt
+                : ""}
             </Text>
-            <Text
-              style={styles.cardText}>
+            <Text style={styles.cardText}>
               {GameState.activeCard.text != null
-                  ? GameState.activeCard.text
-                  : ""}
+                ? GameState.activeCard.text
+                : ""}
             </Text>
           </TouchableOpacity>
         </CardFlip>
