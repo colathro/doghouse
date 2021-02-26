a858a00523bb73daf5790ead4e09489517351a15
Matt <mjzent@csbsju.edu>
2021-02-25 20:05:49 -0600

    oops

---
+++ constants/DeckHelp.ts
@@ -0,0 +1,8 @@
+export const DeckHelp = {
+  breeds: "Starting with you, players must come up with unique answers that fit the category. The first to duplicate an answer or run out the timer is sent to the doghouse.",
+  barkOrBite: "Players who have NOT done the action raise their hand and are sent to the doghouse.",
+  dogFight: "Players debate which of the two options they prefer. Call for a vote at any time. The side with the least number of votes is sent to the doghouse. Incase of a tie, all players are sent to the doghouse.",
+  doghouseOrDare: "Decide which unfortunate player must complete the dare. If they refuse or fail to satisfy, they are sent to the doghouse. If they comply, you're in the doghouse!",
+  throwABone: "Select the player that best fits the \"most likely to\" statement and send them to the doghouse.",
+  teachersPet: "This prompt pertains to you. Other players give their best guesses for the answer to the question. After listening to all guesses, choose the player who gave the worst answer and send them to the doghouse.",
+};
